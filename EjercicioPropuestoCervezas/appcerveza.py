8e009a2b42ab2b2e43cc383ee7b26ec50c8b1273
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-29 10:11:27 +0100

    .

---
+++ EjercicioPropuestoCervezas/appcerveza.py
@@ -0,0 +1,10 @@
+# Necesitamos poner nombre a una “cerveza artesana” que hemos
+# creado. Para ello vamos a realizar un programa que solicite al usuario
+# dos palabras (usando dos preguntas personales que le hagas) y
+# devuelva la marca obtenida indicando: “El nombre de tu cerveza es….”
+# seguido del nombre que compondrás.
+
+print("Vamos a crear el nombre de tu cerveza artesana")
+pregunta1 = input("Dime un animal que te guste: ")
+pregunta2 = input("Dime un color que te guste: ")
+print("El nombre de tu cerveza es: ", pregunta1, pregunta2)
