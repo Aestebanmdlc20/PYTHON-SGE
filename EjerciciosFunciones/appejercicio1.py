1e1d7ff12dc30ee83aa1665814f12b4bebaf9289
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-30 10:14:53 +0100

    .

---
+++ EjerciciosFunciones/appejercicio1.py
@@ -0,0 +1,25 @@
+"""
+Escribe una función que reciba un único parámetro numérico y devuelva
+la suma de los números primos desde el 0 hasta dicho número incluído.
+💡 El 0 y el 1 no se consideran primos.
+"""
+
+def es_primo(n):
+    esPrimo = True
+    if n < 2:
+        esPrimo = False
+    for i in range(2, n):
+        if n % i == 0:
+            esPrimo = False
+    return esPrimo
+
+def suma_primos(n):
+    suma = 0
+    for i in range(2, n+1):
+        if es_primo(i):
+            suma += i
+    return suma
+
+numero = 10
+result = suma_primos(numero)
+print(result) # 17
