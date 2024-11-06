bb17f059fb7c87b87585417b8b12fd2b2ad58168
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-11-06 09:57:23 +0100

    .

--- EjercicioObjetos/App.py
+++ EjercicioObjetos/App.py
@@ -9,31 +9,32 @@ apellido = input("Ingrese su apellido: ")
 persona = Persona(nombre, apellido)
 
 cliente = Cliente(persona, 123456789, 0)
-print(f"Enhroabuena {persona.nombre}, ya eres cliente del banco")
+print(f"Enhorabuena {persona.nombre}, ya eres cliente del banco")
 
 while True:
-    os.system("cls") # Limpio la consola
+    os.system("cls")  # Limpio la consola
 
     print("¿Qué desea hacer?")
     print("1. Depositar")
     print("2. Retirar")
     print("3. Consultar saldo")
     print("4. Salir")
-    opcion = int(input("Ingrese una opción: "))
-
-    if opcion == 1:
-        cantidad = float(input("Ingrese la cantidad a depositar: "))
-        cliente.depositar(cantidad)
-        print(f"Su balance actual es de {cliente.balance}")
-    elif opcion == 2:
-        cantidad = float(input("Ingrese la cantidad a retirar: "))
-        cliente.retirar(cantidad)
-        print(f"Su saldo actual es de {cliente.balance}")
-    elif opcion == 3:
-        print(f"Su balance es de: {cliente.balance} euros")
-    elif opcion == 4:
-        print("Gracias por usar nuestros servicios")
-        break
-    else:
-        print("Opción no válida")
-        
+    try:
+        opcion = int(input("Ingrese una opción: "))
+        if opcion == 1:
+            cantidad = float(input("Ingrese la cantidad a depositar: "))
+            cliente.depositar(cantidad)
+            print(f"Su balance actual es de {cliente.balance}")
+        elif opcion == 2:
+            cantidad = float(input("Ingrese la cantidad a retirar: "))
+            cliente.retirar(cantidad)
+            print(f"Su saldo actual es de {cliente.balance}")
+        elif opcion == 3:
+            print(f"Su balance es de: {cliente.balance} euros")
+        elif opcion == 4:
+            print("Gracias por usar nuestros servicios")
+            break
+        else:
+            print("Opción no válida")
+    except ValueError:
+        print("Por favor, ingrese un número válido")
